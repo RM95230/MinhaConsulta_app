7bc9148db975cfe7856945ac972ef1cafe973cd0
GuilhermeRossiKirsten <guirossik10@gmail.com>
2024-09-25 09:52:49 -0300

    logica login

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -22,20 +22,35 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ title: "Tela Inicial" }} // Título personalizável
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: "Acesse sua conta" }} // Título personalizável
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ title: "Cadastrar" }} // Título personalizável
+      />
       <Stack.Screen
         name="ConsultationsList"
         component={ConsultationsListScreen}
+        options={{ title: "Consultas Agendadas" }} // Título personalizável
       />
       <Stack.Screen
         name="ScheduleConsultation"
         component={ScheduleConsultationScreen}
+        options={{ title: "Agendar Consulta" }} // Título personalizável
       />
       <Stack.Screen
         name="ConfirmAppointment"
         component={ConfirmAppointmentScreen}
+        options={{ title: "Confirmação de Agendamento" }} // Título personalizável
       />
     </Stack.Navigator>
   );
